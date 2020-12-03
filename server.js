32300e863491afa562c4124aace04f3fc2d1dab5
bharat1999 <bharatmalik1999@gmail.com>
2020-12-03 23:29:05 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static("public"));
+
+app.get("/",function(req,res)
+{
+    res.render("home");
+})
+app.get("/contact",function(req,res)
+{
+    res.render("contact");
+})
+app.listen(3000,function()
+{
+  console.log("Running on port 3000");
+});
